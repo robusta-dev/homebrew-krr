bf87a8d39d23f3e23237484a095b291e6bbd2c8a
LeaveMyYard <33721692+LeaveMyYard@users.noreply.github.com>
2023-07-19 18:10:19 +0300

    Manual link fix for 1.4.0

--- Formula/krr.rb
+++ Formula/krr.rb
@@ -6,7 +6,7 @@ class Krr < Formula
         url "https://github.com/robusta-dev/krr/releases/download/v1.4.0/krr-macos-latest-v1.4.0.zip"
         sha256 "d8cd5ea5097d1d13129feba0906fe9ab8f36ad8c12a97d3d4d963900a91bd733"
     elsif OS.linux?
-        url "https://github.com/robusta-dev/krr/releases/download/v1.4.0/krr-linux-latest-v1.4.0.zip"
+        url "https://github.com/robusta-dev/krr/releases/download/v1.4.0/krr-ubuntu-latest-v1.4.0.zip"
         sha256 "3d610afa317c449422ca3b9eb4aafe5328046a7663dbecbce3e478d425dd7e9b"
     end
   
