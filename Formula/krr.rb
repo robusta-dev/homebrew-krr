7b7df3a97add94d604bc735e601ecba8f3650b8e
GitHub Action <action@github.com>
2023-08-08 15:07:16 +0000

    Update formula for release

--- Formula/krr.rb
+++ Formula/krr.rb
@@ -3,11 +3,11 @@ class Krr < Formula
     homepage "https://github.com/robusta-dev/krr"
   
     if OS.mac?
-        url "https://github.com/robusta-dev/krr/releases/download/v1.5.0-pre1/krr-macos-latest-v1.5.0-pre1.zip"
-        sha256 "dd945d0b13c9d3b1bf5c59113f797fc56490dea2c12cfcd99b0493917c7530d7"
+        url "https://github.com/robusta-dev/krr/releases/download/v1.5.1-pre/krr-macos-latest-v1.5.1-pre.zip"
+        sha256 "4adc2045a313fba298349703dac3ce479271a401b96876430a49f98469d87103"
     elsif OS.linux?
-        url "https://github.com/robusta-dev/krr/releases/download/v1.5.0-pre1/krr-ubuntu-latest-v1.5.0-pre1.zip"
-        sha256 "18e90fdab29e5cfa05b2699b46641cff614442147f19c4c8029e1c096bb3a6e0"
+        url "https://github.com/robusta-dev/krr/releases/download/v1.5.1-pre/krr-ubuntu-latest-v1.5.1-pre.zip"
+        sha256 "6b9a8ac0909f704e253a23d83f7e8de8d5401d2c02feaa7987bace6457383da3"
     end
   
     def install
