4c8ea8b164cb71a773b9afc8252eb0e2de225739
Robusta Runner <aantny@gmail.com>
2024-06-01 09:53:46 +0300

    print message to user so they understand why brew install is hanging

--- Formula/krr.rb
+++ Formula/krr.rb
@@ -14,7 +14,8 @@ class Krr < Formula
         libexec.install Dir["*"]
         bin.write_exec_script (libexec/"krr")
         # our binaries are built with pyinstaller and the first executable run is very slow because it unzips packages
-        # to work around that, we "warm up" the binary here during installation so it is fast when the user runs it for the first time
+        # to work around that, we "warm up" the binary here during installation so it is fast when the user runs it for the first timeo
+        ohai "Running 'krr version' to warm up binary and extract compressed python libraries... this can take up to 60 seconds"
         system libexec/"krr", "version"
     end
     
